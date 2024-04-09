493710d605d6134e4792ad22cc7ab81e48320708
Harish Mahato <harishkr999999@gmail.com>
2024-04-09 21:32:33 +0530

    Accordation app completed

--- freeCodeCamp-25-reactProjects/src/components/accordian/index.jsx
+++ freeCodeCamp-25-reactProjects/src/components/accordian/index.jsx
@@ -1,71 +1,59 @@
+import { useState } from "react";
+import data from "./data";
+import './styles.css';
 
-// import { useState } from "react";
-// import data from './data';
-
-// export default function Accordian() {
-    
-//     const [selected, setSelected] = useState(null);
-
-//     function handleSingleSelection(getCurrentId) {
-//         console.log(getCurrentId);
-//         setSelected(getCurrentId===selected?null:getCurrentId);
-//     }
-
-//     return (
-//         <div className="wrapper">
-//             <div className="accordian">
-//                 {
-//                     data && data.length > 0 ?
-//                         data.map(dataItem => <div className="item" key={dataItem.id}>
-//                             <div className="title" onClick={()=>handleSingleSelection(dataItem.id)}>
-//                                 <h3>{dataItem.question}</h3>
-//                                 <span>+</span>
-//                             </div>
-//                             {
-//                                 selected === dataItem.id ?
-//                                     <div className="content">{dataItem.answer}</div>:null
-//                             }
-//                     </div>) : <div>
-//                         Data not found!
-//                     </div>
-//                 }
-//             </div>
-//         </div>
-//     )
-// }
-
-import "./styles.css";
-import {useState} from "react";
-import data from './data';
-
-export default function Accordian() {
+export default function Accordation() {
 
     const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
 
-    function handleButtonClick(getCurrentId){
+    console.log(enableMultiSelection);
+    function handleSingleClick(getCurrentId) {
+        setSelected(selected===getCurrentId?null:getCurrentId)
         console.log(getCurrentId);
-        setSelected(getCurrentId===selected?null:getCurrentId)
     }
+
+    function handleMultiSelection(getCurrentId) {
+        const copyMultiple = [...multiple];
+        let findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+
+        console.log(findIndexOfCurrentId);
+
+        if (findIndexOfCurrentId === -1) copyMultiple.push(getCurrentId)
+        else copyMultiple.splice(findIndexOfCurrentId, 1)
+        
+        setMultiple(copyMultiple);
+    }
+
+    console.log(selected, multiple);
 
     return (
         <div className="wrapper">
-            <button>Enable Multi Selection </button>
-            <div className="accordian">
+            <button onClick={()=>setEnableMultiSelection(!enableMultiSelection)}>Enable Multiselection</button>
+            <div className="accordation">
                 {
-                    data && data.length > 0 ? 
-                        data.map(dataItem =>
+                    data && data.length > 0 ?
+                        data.map(dataItem => (
                             <div className="item" key={dataItem.id}>
-                                <div className="title" onClick={()=>handleButtonClick(dataItem.id)}>
+                                <div className="title" onClick={enableMultiSelection ?
+                                    () => handleMultiSelection(dataItem.id):
+                                    () => handleSingleClick(dataItem.id)}>
                                     <h3>{dataItem.question}</h3>
                                     <span>+</span>
                                 </div>
                                 {
-                                    selected===dataItem.id? dataItem.answer:null
+                                    enableMultiSelection ?
+                                        multiple.indexOf(dataItem.id) !== -1 && <div>{dataItem.answer}</div> :
+                                        selected === dataItem.id && <div>{dataItem.answer }</div>
                                 }
-                            </div>
-                        )
-                    :
-                    <div>Data not found</div>
+
+                                {/* {
+                                    selected===dataItem.id || multiple.indexOf(dataItem.id)? dataItem.answer:null
+                                } */}
+                        </div>
+                    ))
+                    : <div>Data Not Found</div>
                 }
             </div>
         </div>
