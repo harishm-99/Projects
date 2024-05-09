9675b2e96820c7860ed4da138dc1728c0d4d1fcd
Harish Mahato <harishkr999999@gmail.com>
2024-05-09 15:27:42 +0530

    Contact.jsx

--- chat-app/public/src/components/Contacts.jsx
+++ chat-app/public/src/components/Contacts.jsx
@@ -1,8 +1,17 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Logo from './assets/logo.svg'
 
-function Contacts({contacts}) {
+function Contacts({ contacts, currentUser }) {
+
+  const [currentUserName, setCurrentUserName] = useState(undefined);
+  const [currentUserImage, setCurrentUserImage] = useState(undefined);
+
+  useEffect(() => {
+    
+  }, [currentUser])
   return (
-    <div>Contacts</div>
+
   )
 }
 
